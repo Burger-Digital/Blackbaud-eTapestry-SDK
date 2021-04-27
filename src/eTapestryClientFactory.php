b859c1fa13f97efa3c1eced4e0a48b1e0d19bb68
Nathan Heinrich <nathan@fweb.com.au>
2021-04-27 12:27:48 +1000

    update handler code

--- src/eTapestryClientFactory.php
+++ src/eTapestryClientFactory.php
@@ -4,6 +4,9 @@ namespace BurgerDigital\eTapestry;
 
 use BurgerDigital\eTapestry\eTapestryClient;
 use BurgerDigital\eTapestry\eTapestryClassmap;
+use Http\Adapter\Guzzle7\Client as GuzzleAdapter;
+use Phpro\SoapClient\Middleware\RemoveEmptyNodesMiddleware;
+use Phpro\SoapClient\Soap\Handler\HttPlugHandle;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
@@ -13,10 +16,18 @@ class eTapestryClientFactory
 
     public static function factory(string $wsdl) : \BurgerDigital\eTapestry\eTapestryClient
     {
-        $engine = ExtSoapEngineFactory::fromOptions(
-            ExtSoapOptions::defaults($wsdl, [])
-                ->withClassMap(eTapestryClassmap::getCollection())
+        $handler = HttPlugHandle::createForClient(
+            GuzzleAdapter::createWithConfig(['headers' => ['User-Agent' => 'testing/1.0']])
         );
+
+        $handler->addMiddleware(new RemoveEmptyNodesMiddleware());
+
+        $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+            ExtSoapOptions::defaults($wsdl, [])
+                ->withClassMap(eTapestryClassmap::getCollection()),
+            $handler
+        );
+
         $eventDispatcher = new EventDispatcher();
 
         return new eTapestryClient($engine, $eventDispatcher);
